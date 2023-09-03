f715f89cfd5e4b65c450ddf4fa5903b7ddcd51d5
Gururaj KL <gururaj.kl@sasken.com>
2023-09-03 17:06:51 +0530

    Added All The Features

--- ColorPicker/MainPage.xaml.cs
+++ ColorPicker/MainPage.xaml.cs
@@ -1,10 +1,58 @@
-﻿namespace ColorPicker
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace ColorPicker
 {
     public partial class MainPage : ContentPage
     {
+        bool isRandom = false;
+        string hexValue;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void SliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (!isRandom)
+            {
+                var red = sldRed.Value;
+                var green = sldGreen.Value;
+                var blue = sldBlue.Value;
+
+                Color color = Color.FromRgb(red, green, blue);
+                SetColorAndHexValue(color);
+            }
+        }
+
+        private void SetColorAndHexValue(Color color)
+        {
+            Container.BackgroundColor = color;
+            hexValue = color.ToHex();
+            lblHex.Text = hexValue;
+        }
+
+        private void RandomColor(object sender, EventArgs e)
+        {
+            isRandom = true;
+            Random randomValue = new Random();
+            var redColor = randomValue.Next(0, 256);
+            var greenColor = randomValue.Next(0, 256);
+            var blueColor = randomValue.Next(0, 256);
+
+            Color randomColor = Color.FromRgb(redColor, greenColor, blueColor);
+            sldRed.Value = randomColor.Red;
+            sldGreen.Value = randomColor.Green;
+            sldBlue.Value = randomColor.Blue;
+            SetColorAndHexValue(randomColor);
+            isRandom = false;
+        }
+
+        private async void CopyClick(object sender, EventArgs e)
+        {
+            await Clipboard.SetTextAsync(hexValue);
+            await Toast.Make($"{hexValue} Hex value copied", ToastDuration.Short).Show();
         }
     }
 }
--- ColorPicker/MauiProgram.cs
+++ ColorPicker/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace ColorPicker
 {
@@ -7,16 +8,15 @@ namespace ColorPicker
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
+            builder.UseMauiApp<App>().UseMauiCommunityToolkit();
+            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
